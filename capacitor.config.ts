d23b106ba732aa9d5984a3af31f5a4d0c94416de
prakashbhaitakws-dotcom <prakashbhaitakws@gmail.com>
2026-08-23 14:20:31 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,3 +1,3 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = { appId: 'com.tjxshoaib.watermarkremover', appName: 'TJ X Shoaib Watermark Remover', webDir: '.' };
+const config: CapacitorConfig = { appId: 'com.tjxshoaib.watermarkremover', appName: 'TJ X Shoaib Watermark Remover', webDir: 'www' };
 export default config;
